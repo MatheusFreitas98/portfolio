51a92560134992739e977f6e89dcdf038cceac22
Matheus Freitas <65985740+MatheusFreitas98@users.noreply.github.com>
2022-05-07 12:13:16 -0300

    :package: adicionada sessões conhecimento, projetos e orçamento

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.querySelector('.hamburguer').addEventListener('click', () => 
     document.querySelector('.container').classList.toggle('show-menu')
 )
+
+document.querySelector('#nome').addEventListener('click', checkTheBox)
+
+function checkTheBox() {
+    var nome = document.getElementById('nome').type
+}
+
+
+console.log(nome)
